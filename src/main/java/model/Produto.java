c185c0c177e0008a48ca5668a7455f0e98c3b2ab
Luís Fillipe Magalhães <90793038+Luis-Fillipe@users.noreply.github.com>
2022-04-28 23:46:06 -0300

    Add files via upload

---
+++ src/main/java/model/Produto.java
@@ -0,0 +1,72 @@
+package model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class Produto {
+	private int id;
+	private String descricao;
+	private float preco;
+	private int quantidade;
+	
+	public Produto() {
+		id = -1;
+		descricao = "";
+		preco = 0.00F;
+		quantidade = 0;
+	}
+
+	public Produto(int id, String descricao, float preco, int quantidade) {
+		setId(id);
+		setDescricao(descricao);
+		setPreco(preco);
+		setQuantidade(quantidade);
+	}		
+	
+	public int getID() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+	
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	/**
+	 * Método sobreposto da classe Object. É executado quando um objeto precisa
+	 * ser exibido na forma de String.
+	 */
+	@Override
+	public String toString() {
+		return "Produto: " + descricao + "   Preço: R$" + preco + "   Quantidade.: " + quantidade;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		return (this.getID() == ((Produto) obj).getID());
+	}	
+}
